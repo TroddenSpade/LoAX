c3fa3f5bc24c356a2c29b4e8bf866500c62dd316
troddenspade <parsasam1378@gmail.com>
2019-01-25 15:28:17 +0330

    user_register added to rootReducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
-import reducer from './reducer';
+import userRegister from './reducer';
 
 const rootReducer = combineReducers({
-    reducer
+    userRegister,
 });
 
 export default rootReducer;
