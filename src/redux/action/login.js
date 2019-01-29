e36a595cb01c307a55c55d4023fd06a88c785850
troddenspade <parsasam1378@gmail.com>
2019-01-29 23:45:05 +0330

    index renamed to login , registerUser and signinUser actions added

--- src/redux/action/login.js
+++ src/redux/action/login.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 const API =`AIzaSyDt3RD0Jh7L_KVfh8wj765XPveyeeLEc0c`;
+const FireBase=`https://loax-70d8e.firebaseio.com/`;
 const Register =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=`;
+const Signin =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=`;
 
 export const registerUser =(data)=>{
     const request = axios({
@@ -18,6 +20,26 @@ export const registerUser =(data)=>{
 
     return{
         type:"user_register",
+        payload:request,
+    }
+}
+
+export const signinUser=(data)=>{
+    const request = axios({
+        method:"POST",
+        url:`${Signin}${API}`,
+        data:{
+            email:data.email,
+            password:data.password,
+            returnSecureToken:true,
+        },
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(response => response.data);
+
+    return{
+        type:"user_signin",
         payload:request,
     }
 }
