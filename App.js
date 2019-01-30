e8e8b93fca130caec87e6282a63e5e9ad9164c44
troddenspade <parsasam1378@gmail.com>
2019-01-30 23:53:45 +0330

    main.js imported and Main component renders

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import Store from './src/redux/store'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import reducer from './src/redux/reducer';
-import Signup from './src/screens/singup';
+import Main from './src/main';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Signup/>
-      </View>
-
-    );
+      <Main/>
+    )
   }
 }
 
