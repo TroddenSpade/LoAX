14da77aa7a393286e5df66a385ef6ef0f7edf49f
troddenspade <parsasam1378@gmail.com>
2019-01-31 23:54:14 +0330

    refresh_token action added

--- src/redux/action/login.js
+++ src/redux/action/login.js
@@ -3,6 +3,7 @@ const API =`AIzaSyDt3RD0Jh7L_KVfh8wj765XPveyeeLEc0c`;
 const FireBase=`https://loax-70d8e.firebaseio.com/`;
 const Register =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=`;
 const Signin =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=`;
+const Exchange =`https://securetoken.googleapis.com/v1/token?key=`;
 
 export const registerUser =(data)=>{
     const request = axios({
@@ -40,6 +41,22 @@ export const signinUser=(data)=>{
 
     return{
         type:"user_signin",
+        payload:request,
+    }
+}
+
+export const exchangeToken=(refreshToken)=>{
+    const request = axios({
+        method:"POST",
+        url:`${Exchange}${API}`,
+        data:"grant_type=refresh_token&refresh_token="+refreshToken,
+        headers:{
+            "Content-Type":"application/x-www-form-urlencoded"
+        }
+    }).then(response => response.data);
+
+    return{
+        type:"exchange_token",
         payload:request,
     }
 }
