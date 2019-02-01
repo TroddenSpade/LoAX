d28acae61f055f9a7e95ccf5fb2a44f89f8d6d0f
troddenspade <parsasam1378@gmail.com>
2019-02-01 11:43:19 +0330

    reducers initial values changed to false

--- src/redux/reducer/login.js
+++ src/redux/reducer/login.js
@@ -3,18 +3,26 @@ export default login = (state={},action)=>{
         case 'user_register':
             return {
                 newUser:{
-                    userid:action.payload.localId,
-                    token:action.payload.idToken,
-                    refreshToken:action.payload.refreshToken,
+                    userid:action.payload.localId || false,
+                    token:action.payload.idToken || false,
+                    refreshToken:action.payload.refreshToken || false,
                 }
             }
         case 'user_signin':
             return {
                 userData:{
-                    userid:action.payload.localId,
-                    token:action.payload.idToken,
-                    refreshToken:action.payload.refreshToken,
+                    userid:action.payload.localId || false,
+                    token:action.payload.idToken || false,
+                    refreshToken:action.payload.refreshToken || false,
                 }
+            }
+        case 'exchange_token':
+            return{
+                userData:{
+                    userid:action.payload.user_id || false,
+                    token:action.payload.id_token || false,
+                    refreshToken:action.payload.refresh_token || false,
+                } 
             }
         default :
             return state;
