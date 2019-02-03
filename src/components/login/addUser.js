4a3056fd278a078e17348dbcaac7cb33048df01a
troddenspade <parsasam1378@gmail.com>
2019-02-03 22:59:32 +0330

    axios method changed to pT

--- src/components/login/addUser.js
+++ src/components/login/addUser.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
-const FireBaseUser=`https://loax-70d8e.firebaseio.com/user.json`;
+const FireBaseUser=`https://loax-70d8e.firebaseio.com/user`;
 
-export const addUser =(state,store)=>{
-    
-    axios({
-        method:"POST",
-        url:`${FireBaseUser}?auth=${store.token}`,
-        data:{
+export const addUser=(state,store)=>{
+    request = axios({
+        method: 'PUT',
+        url: `${FireBaseUser}/${state.username}.json?auth=${store.token}`,
+        data: {
             username:state.username,
             email:state.email,
             userid:store.userid,
             refreshToken:store.refreshToken,
-        }
-    }).then(response => response.data)
-        .catch(e=>{console.log(e)})
+        },
+        config: { headers: {'Content-Type': 'multipart/form-data' }}
+    }).then(()=>console.log(request))
 }
