200d5bb3c75ed679f5c31a121bbdb78fce6882bc
troddenspade <parsasam1378@gmail.com>
2019-02-10 22:00:16 +0330

    app.js extra stuff deleted

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import Store from './src/redux/store'
-import { StyleSheet } from 'react-native';
 
-import reducer from './src/redux/reducer';
 import Main from './src/main';
 
 class App extends React.Component {
+
   render() {
-    return (
-      <Main/>
-    )
+      return (
+          <Provider store={Store}>
+            <Main/>
+          </Provider>
+      );
   }
 }
 
-export default reduxApp = ()=>{
-  return(
-    <Provider store={Store(reducer)}>
-      <App/>
-    </Provider>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
