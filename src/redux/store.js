ea0eb58d560c7c8ec604eb2e4ba7446643ba1cd9
troddenspade <parsasam1378@gmail.com>
2019-02-10 21:47:32 +0330

    thunk added to middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import {createStore , applyMiddleware} from 'redux';
 import promiseMiddleware from "redux-promise";
+import thunk from 'redux-thunk';
+import rootReducer from './reducer/index';
 
-const Store = applyMiddleware(promiseMiddleware)(createStore);
+const Store = createStore(rootReducer,applyMiddleware(thunk,promiseMiddleware));
 
 export default Store;
