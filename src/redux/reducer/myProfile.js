396ea5c3a4674a561f550ba8ef4aee3cb667d3e6
troddenspade <parsasam1378@gmail.com>
2019-02-14 23:27:08 +0330

    my Profile reducers added

---
+++ src/redux/reducer/myProfile.js
@@ -0,0 +1,26 @@
+export default myProfile =(state={postsLoading:true,profileLoading:true},action)=>{
+    switch(action.type){
+        case 'MY_POSTS_SUCCESSFUL':
+            return{
+                ...state,
+                myPosts:action.payload,
+                postsLoading:false
+            }
+
+        case 'MY_DATA_SUCCESSFUL':
+            return{
+                ...state,
+                myData:action.payload,
+                profileLoading:false
+            }
+
+        case 'PROFILE_SUCCESSFUL':
+            return{
+                ...state,
+                profile:action.payload,
+            }
+
+        default:
+            return state
+    }
+}
