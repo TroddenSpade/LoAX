62347ec7a1873807724fe96e43b86781c718072e
troddenspade <parsasam1378@gmail.com>
2019-02-15 23:39:53 +0330

    firebase get req replaced with axios

--- src/redux/action/getPosts.js
+++ src/redux/action/getPosts.js
@@ -1,36 +1,79 @@
-import axios from 'axios';
+import * as firebase from 'firebase';
 
-const FireBase=`https://loax-70d8e.firebaseio.com`;
+const firebaseConfig = {
+    apiKey: "AIzaSyDt3RD0Jh7L_KVfh8wj765XPveyeeLEc0c",
+    authDomain: "loax-70d8e.firebaseapp.com",
+    databaseURL: "https://loax-70d8e.firebaseio.com",
+    storageBucket: "loax-70d8e.appspot.com"
+};
+  
+firebase.initializeApp(firebaseConfig);
 
-export const getPosts=()=>{
+
+export const getPosts=(lastKey)=>{
     // if(tag==null){
     //     let URL = `${FireBase}/post.json`;
     // }else{
     //     let URL =`${FireBase}`;
     // }
-    
-    return async (dispatch , getState)=>{
-        let URL = `https://loax-70d8e.firebaseio.com/post.json`;
-        let posts=[];
-        dispatch({type:'START_LOADING'});
-        await axios(URL).then(
-        async (response)=>{
-            for(let key in response.data){
-                let URL = `https://loax-70d8e.firebaseio.com/user/${response.data[key].userid}.json`
-                await axios(URL)
-                .then(async (response2) => {
-                    posts.push({
-                        ...response.data[key],
-                        "username":response2.data.username,
-                        "avatar":response2.data.pic,
+
+    if(lastKey == null){
+        return async (dispatch , getState)=>{
+            let posts=[];
+            let arrayOfKeys =[];
+            dispatch({type:'START_LOADING'});
+            firebase.database().ref(`post`)
+            .orderByKey()
+            .limitToFirst(5)
+            .once(`value`)
+            .then(async (snapshot) => {
+                for(let key in snapshot.val()){
+                    await firebase.database().ref(`user/${snapshot.val()[key].userid}`)
+                    .once(`value`)
+                    .then(async (snapshot2)=>{
+                        posts.push({
+                            ...snapshot.val()[key],
+                            "username":snapshot2.val().username,
+                            "avatar":snapshot2.val().pic,
+                        })
+                        return snapshot2
                     })
-                    // response.data[key].username =  response2.data.username;
-                    // response.data[key].avatar =  response2.data.pic;
-                    return response2;
-                })
-            }
-            return posts;
-        }).then((response)=>dispatch({type:'POSTS_SUCCESS',payload:response}))
-        .catch((e)=>dispatch({type:'POSTS_ERROR',error:e}))
+                }
+                arrayOfKeys= Object.keys(snapshot.val())
+                return posts
+            }).then((response)=>dispatch({type:'POSTS_SUCCESS',payload:response,lastKey:arrayOfKeys[4]}))
+            .catch((e)=>dispatch({type:'POSTS_ERROR',error:e}))
+            
+        }
+    }else{
+        return async (dispatch , getState)=>{
+            let posts=[];
+            let arrayOfKeys =[];
+            dispatch({type:'START_LOADING'});
+            firebase.database().ref(`post`)
+            .orderByKey()
+            .startAt(lastKey)
+            .limitToFirst(5)
+            .once(`value`)
+            .then(async (snapshot) => {
+                for(let key in snapshot.val()){
+                    await firebase.database().ref(`user/${snapshot.val()[key].userid}`)
+                    .once(`value`)
+                    .then(async (snapshot2)=>{
+                        posts.push({
+                            ...snapshot.val()[key],
+                            "username":snapshot2.val().username,
+                            "avatar":snapshot2.val().pic,
+                        })
+                        return snapshot2
+                    })
+                }
+                arrayOfKeys= Object.keys(snapshot.val())
+                return posts
+            }).then((response)=>dispatch({type:'POSTS_SUCCESS',payload:response,lastKey:arrayOfKeys[4]}))
+            .catch((e)=>dispatch({type:'POSTS_ERROR',error:e}))
+            
+        }
     }
+    
 }
