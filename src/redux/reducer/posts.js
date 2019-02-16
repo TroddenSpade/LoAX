76344e6fd1e32b3aae8baf107803c0a90388c4a8
troddenspade <parsasam1378@gmail.com>
2019-02-16 19:52:23 +0330

    POSTS_ADDED case for adding more posts

--- src/redux/reducer/posts.js
+++ src/redux/reducer/posts.js
@@ -4,7 +4,11 @@ export default posts=(state={loading:true},action)=>{
             return Object.assign({},state,{loading:true})
 
         case 'POSTS_SUCCESS':
-                return Object.assign({}, state, { list: action.payload, loading: false,lastKey:action.lastKey })
+            return Object.assign({}, state, { list:action.payload , loading: false,lastKey:action.lastKey })
+
+        case 'POSTS_ADDED':
+            let arr= state.list.concat(action.payload);
+            return Object.assign({},state,{list:arr,lastKey:action.lastKey});
 
         case 'POSTS_ERROR':
             return Object.assign({},state,{loading:true})
