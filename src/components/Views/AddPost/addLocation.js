c9b4985eab820b5a9e877463e8585b59e445ee31
troddenspade <parsasam1378@gmail.com>
2019-02-18 23:17:12 +0330

    getPermission func updated

--- src/components/Views/AddPost/addLocation.js
+++ src/components/Views/AddPost/addLocation.js
@@ -1,64 +1,90 @@
 import React from 'react';
-import { View,Text } from 'react-native';
-import { Permissions,Location,MapView } from 'expo';
+import { Permissions,Location,MapView,IntentLauncherAndroid } from 'expo';
 
 import Loading from '../../screens/loading';
 
 export default class AddLocation extends React.Component{
-    state={
-        location:{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          },
-        isMyLocation:false,
-        permission:null,
-    };
+
+    constructor(props) {
+        super(props);
+        const { navigation } = props;
+        const location = navigation.getParam('location');
+        this.state = {
+            loading:true,
+            location:location,
+            providerStatus:null,
+        };
+    }
 
     getLocationPermission = async ()=>{
         let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        console.log(status)
         if (status !== 'granted') {
             this.setState({
                 permission: 'Permission To Access Location Was Denied',
             });
         }
-        let location = await Location.getCurrentPositionAsync().then(response=>response.coords)
-        this.setState({
-            isMyLocation:true,
-            location:{
-                latitude: location.latitude,
-                longitude: location.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-        } });
-        
+        let providerStatus = await Location.getProviderStatusAsync();
+        this.setState({providerStatus});
+
+        if(!this.state.providerStatus.gpsAvailable || !this.state.providerStatus.locationServicesEnabled){
+            IntentLauncherAndroid.startActivityAsync(IntentLauncherAndroid.ACTION_LOCATION_SOURCE_SETTINGS);
+        }
+        providerStatus = await Location.getProviderStatusAsync();
+        this.setState({providerStatus});
+
+        if(this.state.providerStatus.gpsAvailable && this.state.providerStatus.locationServicesEnabled){
+            if(this.state.providerStatus.networkAvailable){
+                let location = await Location.getCurrentPositionAsync().then(response=>response.coords)
+                this.setState({
+                    loading:false,
+                    location:{
+                        latitude: location.latitude,
+                        longitude: location.longitude,
+                        latitudeDelta: 0.0202,
+                        longitudeDelta: 0.0149,
+                    } 
+                });
+            }else{
+                alert("Can't Connect to Server !");
+                this.setState({
+                    loading:false,
+                })
+            }
+        }
+        this.setState({loading:false})
     };
 
     handleLocationChange=(location)=>{
         this.setState({location});
-        this.props.locationHandler(location);
+    }
+
+    submitLocation=()=>{
+        const { navigation } = this.props;
+        const locationHandler = navigation.getParam('locationHandler');
+        locationHandler(this.state.location);
     }
 
     componentWillMount(){
         this.getLocationPermission();
+
     }
 
     render(){
-        if (this.state.isMyLocation){
+        if (this.state.loading){
+            return <Loading/>
+        }else{
             return(
                 <MapView
                 style={{ flex: 1 }}
                 region={this.state.location}
-                onRegionChange={this.handleLocationChange}>
+                onRegionChangeComplete={this.handleLocationChange}>
                     <MapView.Marker
-                    coordinate={this.state.location}/>
+                    title="Click Me"
+                    coordinate={this.state.location}
+                    onCalloutPress={this.submitLocation}
+                    />
                 </MapView>
             )
-        }else{
-            return <Loading/>
         }
-        
     };
 }
