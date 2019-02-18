d84ebe26d0fb610e1d190f5c2ddafb0a1dccf335
troddenspade <parsasam1378@gmail.com>
2019-02-18 23:18:23 +0330

    Buttons and UseCamera Function added

--- src/components/Views/AddPost/addPicture.js
+++ src/components/Views/AddPost/addPicture.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet,Button } from 'react-native';
+import { Permissions,ImagePicker } from 'expo';
 
 export default class AddPicture extends React.Component{
+
+    state={
+        isPhoto: false,
+    }
+
+    useCamera= async ()=>{
+        await Permissions.askAsync(Permissions.CAMERA);
+        await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        response = await ImagePicker.launchCameraAsync()
+        console.log(response);
+    }
+
+    useGallery=()=>{
+
+    }
+
     render(){
-        return(
-            <View><Text>picture add</Text></View>
-        )
+        if(this.state.isPhoto){
+            return(
+                <View><Text>yess</Text></View>
+            )
+        }else{
+            return(
+            <View style={styles.container}>
+                <Button
+                style={{padding: 5}}
+                onPress={this.useCamera}
+                title="take a photo"
+                color="lightgreen"/>
+                
+                <Button
+                style={{padding: 5}}
+                onPress={this.useGallery}
+                title="Choose from Gallery"
+                color="lightgreen"/>
+            </View>
+            )
+        }
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+});
