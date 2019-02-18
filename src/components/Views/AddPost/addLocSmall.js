88f5b7f58915e62b600f66f0c7b1941aecf80e4e
troddenspade <parsasam1378@gmail.com>
2019-02-18 23:08:01 +0330

    small mapView adde to addPost View

---
+++ src/components/Views/AddPost/addLocSmall.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+import { Permissions,Location,MapView } from 'expo';
+
+import Loading from '../../screens/loading';
+
+export default class AddLocSmall extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props,
+        };
+    }
+
+    getLocationPermission = async ()=>{
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                permission: 'Permission To Access Location Was Denied',
+            });
+        }
+        // let location = await Location.getCurrentPositionAsync().then(response=>response.coords)
+        this.setState({
+            isMyLocation:true,
+            
+        });
+    };
+
+    submitLocation=()=>{
+        const { navigation } = this.props;
+        const locationHandler = navigation.getParam('locationHandler');
+        locationHandler(this.state.location);
+    }
+
+    componentWillMount(){
+        this.getLocationPermission();
+    }
+
+    render(){
+
+        if (this.state.isMyLocation){
+            return(
+                <MapView
+                style={{ flex: 1 }}
+                region={this.props.location}>
+                    <MapView.Marker
+                    title="Click Me"
+                    coordinate={this.props.location}
+                    onCalloutPress={this.submitLocation}
+                    />
+                </MapView>
+            )
+        }else{
+            return <Loading/>
+        }
+        
+    };
+}
