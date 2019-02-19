5b175f5d0333cc00f6ec9097d0a868d0ec262ec8
troddenspade <parsasam1378@gmail.com>
2019-02-19 23:51:46 +0330

    adding photos using camera and gallery

--- src/components/Views/AddPost/addPicture.js
+++ src/components/Views/AddPost/addPicture.js
@@ -1,44 +1,106 @@
 import React from 'react';
-import { View,Text,StyleSheet,Button } from 'react-native';
+import { 
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    Image,
+    TouchableOpacity,
+    ImageBackground,
+} from 'react-native';
 import { Permissions,ImagePicker } from 'expo';
 
 export default class AddPicture extends React.Component{
 
-    state={
-        isPhoto: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props,
+            image:null,
+            cancelled:true,
+        };
     }
 
     useCamera= async ()=>{
         await Permissions.askAsync(Permissions.CAMERA);
         await Permissions.askAsync(Permissions.CAMERA_ROLL);
-        response = await ImagePicker.launchCameraAsync()
-        console.log(response);
+        response = await ImagePicker.launchCameraAsync({
+            allowsEditing:true,
+            aspect:[1,1],
+        })
+        if(response.cancelled == false){
+            this.setState({
+                cancelled:response.cancelled,
+                image:response.uri,TouchableOpacity
+            })
+            this.props.imageHandler(response.uri);
+        }
     }
 
-    useGallery=()=>{
+    useGallery= async ()=>{
+        await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        response = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing:true,
+            aspect: [1,1],
+        })
+        console.log(response)
 
+        if(response.cancelled == false){
+            this.setState({
+                cancelled:response.cancelled,
+                image:response.uri,
+            })
+            this.props.imageHandler(response.uri);
+        }
     }
 
     render(){
-        if(this.state.isPhoto){
-            return(
-                <View><Text>yess</Text></View>
-            )
-        }else{
+        if(this.state.cancelled){
             return(
             <View style={styles.container}>
+                <View style={{padding: 5}}>
                 <Button
-                style={{padding: 5}}
                 onPress={this.useCamera}
                 title="take a photo"
                 color="lightgreen"/>
-                
+                </View>
+
+                <View style={{padding: 5}}>
                 <Button
-                style={{padding: 5}}
                 onPress={this.useGallery}
                 title="Choose from Gallery"
                 color="lightgreen"/>
+                </View>  
             </View>
+            )
+        }else{
+            return(
+            <ImageBackground source={{uri:this.state.image}} style={{width: '100%', height:this.props.height}} blurRadius={1}>
+            <View style={styles.container}>
+
+                <View style={{padding: 5}}>
+                <Button
+                onPress={this.useCamera}
+                title="Camera"
+                color="lightgreen"/>
+                </View>
+
+                <View>
+                <Image
+                style={{height:this.state.height,width:this.state.height,borderRadius:10}}
+                source={{uri:this.state.image}}
+                />
+                </View>
+
+                <View style={{padding: 5}}>
+                <Button
+                onPress={this.useGallery}
+                title="Gallery"
+                color="lightgreen"/>
+                </View> 
+                
+            </View>
+            </ImageBackground>
             )
         }
     }
@@ -48,7 +110,7 @@ const styles = StyleSheet.create({
     container:{
       flex:1,
       flexDirection: "row",
-      justifyContent: "center",
+      justifyContent: "space-around",
       alignItems: "center",
     },
 });
