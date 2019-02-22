765605451e308c95a87162133626f7f7548bc81d
troddenspade <parsasam1378@gmail.com>
2019-02-22 23:49:35 +0330

    signout button and list function added

--- src/components/Views/profile/profileTab.js
+++ src/components/Views/profile/profileTab.js
@@ -3,46 +3,58 @@ import {
   View, 
   Image,
   Text,
+  ScrollView,
   StyleSheet,
-  Dimensions
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 import { connect } from 'react-redux';
 
+import { removeToken,getTokens } from '../../../utils/misc';
 import ProfileList from './profileList';
 import Loading from '../../screens/loading';
 
 const screenWidth = Dimensions.get("window").width;
-
+const screenHeight = Dimensions.get("window").height;
 
 class Profile extends React.Component {
+
+  list = (data)=>{
+    return data.map((item,id)=>(
+      <ProfileList key={id} data={item}/>
+    ))
+  }
 
   render(){
     if(this.props.postsLoading || this.props.profileLoading) return <Loading/>
     return (
       
-      <View>
-        <View style={styles.profile}>
-          <View style={styles.settings}>
-            <Text>setting</Text>
+      <View style={styles.container}>
+        {/* <View style={styles.profile}> */}
+
+          <View style={styles.profile}>
+            <TouchableOpacity style={styles.settings} 
+            onPress={()=>removeToken(()=>this.props.navigation.navigate('Login'))}>
+              <Text>Sign Out</Text>
+            </TouchableOpacity>
+
+            <View style={styles.avatar}>
+                <View style={{alignItems:'center'}}>
+                <Image style={styles.imagePro} source={{uri :this.props.myData.pic}}/>
+                </View>
+                
+                <View style={styles.username}>
+                <Text style={{fontSize:20,color:'green'}}>{this.props.myData.username}</Text>
+                </View>
+            </View>
           </View>
 
-          <View style={styles.avatar}>
-              <View style={{alignItems:'center'}}>
-              <Image style={styles.imagePro} source={{uri :this.props.myData.pic}}/>
-              </View>
-              
-              <View style={styles.username}>
-              <Text style={{fontSize:20,color:'green'}}>{this.props.myData.username}</Text>
-              </View>
-          </View>
-
+          <ScrollView>
           <View style={styles.bio}>
-            <Text>BIO DOOOOOO DOOOOO LOOOO</Text>
+            <Text>{"hello\nfirstline\nsecond\nthird\nforth\fifth"}</Text>
           </View>
-
-        </View>
-      
-      <ProfileList data={this.props.myposts}/>
+            {this.list(this.props.myposts)}
+          </ScrollView>
 
       </View>
     );
@@ -62,6 +74,11 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps,null)(Profile);
 
 const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },  
   block:{
       height:100,
       borderColor: 'lightgrey',
