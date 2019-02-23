2bc85d40ea044e9c1cd7f5db65a8d6e401ff0ca4
troddenspade <parsasam1378@gmail.com>
2019-02-24 00:25:50 +0330

    search reducer added to root reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -4,12 +4,14 @@ import login from './login';
 import posts from './posts';
 import myProfile from './myProfile';
 import profile from './profile';
+import search from './search';
 
 const rootReducer = combineReducers({
     login,
     posts,
     myProfile,
     profile,
+    search
 });
 
 export default rootReducer;
