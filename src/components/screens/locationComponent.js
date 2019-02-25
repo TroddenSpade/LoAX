7ecca1875c26cb92834ada88c55c1a5029e5c599
troddenspade <parsasam1378@gmail.com>
2019-02-25 22:42:17 +0330

    pullUp bar added to MapView

--- src/components/screens/locationComponent.js
+++ src/components/screens/locationComponent.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 import { MapView } from 'expo';
+import { AntDesign } from '@expo/vector-icons';
 
 export default LocationComponent = (props) =>{
   return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={props.location}>
-      <MapView.Marker
-        coordinate={props.location}/>
-    </MapView>
+    <View style={styles.container}>
+      <MapView
+        style={{height:'100%',width:'100%'}}
+        initialRegion={props.location}>
+        <MapView.Marker
+          coordinate={props.location}/>
+      </MapView>
+      <View style={styles.topBar}>
+        <Text style={styles.pullup}>
+        <AntDesign name="caretup" size={30} color='white'/>
+        PULL UP</Text>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  topBar:{
+    height: 25,
+    width: '100%',
+    flex: 1,
+    backgroundColor: "lightgreen",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    position: 'absolute',
+  },
+  pullup:{
+    color:"white",
+    fontSize: 22,
+  }
+});
