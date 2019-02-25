1d89d03f98f5bf4610a2a20dd1121a40c8e4745d
troddenspade <parsasam1378@gmail.com>
2019-02-25 22:46:39 +0330

    setToken s values  updated

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -4,10 +4,10 @@ export const setTokens =(data)=>{
     const now = new Date();
     const expire = now.getTime() + 60*60*1000;
     AsyncStorage.multiSet([
-        ['@LoAX@token',data.token],
+        ['@LoAX@token',data.idToken],
         ['@LoAX@refereshToken',data.refreshToken],
         ['@LoAX@expireToken',expire.toString()],
-        ['@LoAX@userid',data.userid]
+        ['@LoAX@userid',data.localId]
     ]).then(()=>console.log("Token Has Been Set"));
 }
 
