91528f56655a65c45dffe6d30dc6ee3ad49fda0f
troddenspade <parsasam1378@gmail.com>
2019-03-03 22:57:32 +0330

    modal and dropdownAlert added to SearchScreen

--- src/components/Views/search/search.js
+++ src/components/Views/search/search.js
@@ -12,10 +12,13 @@ import {
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { AntDesign,MaterialCommunityIcons } from '@expo/vector-icons';
+import DropdownAlert from 'react-native-dropdownalert';
+import Modal from "react-native-modal";
 
 import Loading from '../../screens/loading';
 import List from './searchList';
 import { search } from '../../../redux/action/search';
+import { report } from '../../../utils/misc';
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -28,6 +31,9 @@ class Search extends React.Component{
         loadposts:false,
         scrollY:0,
         refreshing:false,
+        modalVisible:false,
+        modalId:null,
+        isReport:false,
     }
 
     data = this.props.posts;
@@ -51,7 +57,8 @@ class Search extends React.Component{
           key={id} posts={item}
           locationHandler={() => this.props.navigation.navigate('Parallax',{data:item})}
           profileHandler={()=>this.props.navigation.navigate('Profile',{data:item.userid})}
-          tagHandler={(tag)=>this.props.navigation.navigate('Search',{tag:tag})}/>
+          tagHandler={(tag)=>this.props.navigation.navigate('Search',{tag:tag})}
+          modalHandler={()=>this.setState({modalVisible:true,modalId:item.id})}/>
         ))
       )
 
@@ -130,7 +137,70 @@ class Search extends React.Component{
                         <AntDesign name="search1" color={"grey"} size={30}/>
                     </TouchableOpacity>
                 </View>
-                
+                <DropdownAlert defaultContainer={{paddingTop: 20,padding: 5}} ref={ref => this.dropdown = ref} />
+                <Modal
+                isVisible={this.state.modalVisible}
+                transparent={true}
+                onBackButtonPress={()=>this.setState({modalVisible:false,isReport:false})}
+                onBackdropPress={()=>this.setState({modalVisible:false,isReport:false})}>
+                    {this.state.isReport ? 
+                    <View style={styles.modalContent}>
+                    <TouchableOpacity
+                    style={styles.goback}
+                    onPress={()=>this.setState({isReport:false})}>
+                    <Text style={{fontSize:15,color:'lightgreen'}}><AntDesign name="back" size={15} color='lightgreen'/>GoBack</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.reports}
+                    onPress={()=>{
+                    report(this.state.modalId,'WRONG_LOCATION',this.props.data,
+                    ()=>this.dropdown.alertWithType('success', 'successfully done!', 'We\'ll get to that! ;)'),
+                    (e)=>this.dropdown.alertWithType('error', 'Error', `error has been occurred :( \n${e} `));
+                    this.setState({isReport:false,modalVisible:false})}
+                    }><Text>Wrong Location</Text></TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.reports}
+                    onPress={()=>{
+                    report(this.state.modalId,'UNRELATED_PICTURE',this.props.data,
+                    ()=>this.dropdown.alertWithType('success', 'successfully done!', 'We\'ll get to that! ;)'),
+                    (e)=>this.dropdown.alertWithType('error', 'Error', `error has been occurred :( \n${e} `));
+                    this.setState({isReport:false,modalVisible:false})}
+                    }><Text>Unrelated Picture</Text></TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.reports}
+                    onPress={()=>{
+                    report(this.state.modalId,'ADVERTISMENT',this.props.data,
+                    ()=>this.dropdown.alertWithType('success', 'successfully done!', 'We\'ll get to that! ;)'),
+                    (e)=>this.dropdown.alertWithType('error', 'Error', `error has been occurred :( \n${e} `));
+                    this.setState({isReport:false,modalVisible:false})}
+                    }><Text>Advertisment</Text></TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.reports}
+                    onPress={()=>{
+                    report(this.state.modalId,'VIOLENCE',this.props.data,
+                    ()=>this.dropdown.alertWithType('success', 'successfully done!', 'We\'ll get to that! ;)'),
+                    (e)=>this.dropdown.alertWithType('error', 'Error', `error has been occurred :( \n${e} `));
+                    this.setState({isReport:false,modalVisible:false})}
+                    }><Text>Violence</Text></TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.reports}
+                    onPress={()=>{
+                    report(this.state.modalId,'NUDITY_PORNOGRAPHY',this.props.data,
+                    ()=>this.dropdown.alertWithType('success', 'successfully done!', 'We\'ll get to that! ;)'),
+                    (e)=>this.dropdown.alertWithType('error', 'Error', `error has been occurred :( \n${e} `));
+                    this.setState({isReport:false,modalVisible:false})}
+                    }><Text>Nudity & Pornography</Text></TouchableOpacity>
+                    </View>
+                    :
+                    <View style={styles.modalContent}>
+                    <TouchableOpacity onPress={()=>alert("it's not available right now !")} style={styles.reports}>
+                        <Text>Share</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>this.setState({isReport:true})} style={styles.reports}>
+                        <Text>Report</Text>
+                    </TouchableOpacity>
+                    </View>}
+                </Modal>
             </View>
         )
     }
@@ -141,6 +211,7 @@ const mapStateToProps =(state)=>{
     return{
         posts:state.search.list,
         loading:state.search.loading,
+        data:state.login.userData,
     }
 }
 
@@ -184,5 +255,32 @@ const styles = StyleSheet.create({
     },
     blank:{
         height:75,
-    }
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 128, 0, 0.1)',
+      },
+      reports:{
+        height:50,
+        width: '100%',
+        margin: 5,
+        borderColor: 'lightgreen',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+      },
+      goback:{
+        height:30,
+        width: '50%',
+        margin: 5,
+        borderColor: 'lightgreen',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderRadius: 10,
+      }
 });
