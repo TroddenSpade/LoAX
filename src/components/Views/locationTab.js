9520f8fda6d347d67df04a083cde8d8578219954
troddenspade <parsasam1378@gmail.com>
2019-03-03 22:55:25 +0330

    construct logo added

--- src/components/Views/locationTab.js
+++ src/components/Views/locationTab.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Location extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Location</Text>
+        <Text>LocationTab is under Construction !</Text>
+        <Ionicons name="md-construct" size={100} color='lightgreen'/>
       </View>
     );
   }
