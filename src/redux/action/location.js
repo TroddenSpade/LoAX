066b900c1c5c6a92fe90d6ca00ff25a9859bdaef
troddenspade <parsasam1378@gmail.com>
2019-03-07 19:21:21 +0330

    username and avatar added to posts data

--- src/redux/action/location.js
+++ src/redux/action/location.js
@@ -3,11 +3,24 @@ import * as firebase from 'firebase';
 export const location =(geoHash)=>{
     return (dispatch,getState)=>{
         dispatch({type:'LOADING_START'});
+        let posts=[];
         firebase.database().ref(`post`)
         .orderByChild(`geoHash`)
         .once(`value`)
-        .then((snapshot) => {
-            return Object.values(snapshot.val());
+        .then(async (snapshot) => {
+            for(let key in snapshot.val()){
+                await firebase.database().ref(`user/${snapshot.val()[key].userid}`)
+                .once(`value`)
+                .then(async (snapshot2)=>{
+                    posts.push({
+                        ...snapshot.val()[key],
+                        "username":snapshot2.val().username,
+                        "avatar":snapshot2.val().pic,
+                    })
+                    return snapshot2
+                })
+            }
+            return posts
         }).then((response)=>dispatch({type:'LOCATION_POSTS_SUCCESS',payload:response}))
         .catch((e)=>console.log(e))
     }
