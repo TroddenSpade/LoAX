e01814f698e97d08136fdb410d036490e9be9fae
troddenspade <parsasam1378@gmail.com>
2019-03-27 02:33:51 +0430

    auto sign in and token validation logics changed

--- src/main.js
+++ src/main.js
@@ -1,73 +1,79 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {
-    createAppContainer,
-    createSwitchNavigator
-} from 'react-navigation';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import { Login } from './components/login';
-import { Views } from './components/Views';
+import { Login } from "./components/login";
+import { Views } from "./components/Views";
 
-import Loading from './components/screens/loading';
-import { getTokens } from './utils/misc';
-import {exchangeToken} from './redux/action/login';
+import Loading from "./components/screens/loading";
+import { getTokens } from "./utils/misc";
+import { checkToken } from "./redux/action/login";
 
-class Main extends React.Component{
-    state={
-        login:null,
-    }
+class Main extends React.Component {
+  state = {
+    login: null
+  };
 
-    componentWillMount(){
-        getTokens((value)=>{
-            if(value[0][1] == null)  this.setState({login:true})
-            else{
-                console.log(value)
-                this.props.exchangeToken(value[1][1])
-                .then(()=>{this.setState({login:false})})
-                .catch(e=>{this.setState({login:false})})
-            }
-        });
-    }
-
-    componentDidUpdate(){
-        if(this.state.login == true){
-            this.props.navigation.navigate('Login')
-
-        }else if(this.state.login == false){
-            this.props.navigation.navigate('Views')
+  componentWillMount() {
+    getTokens(
+      value => {
+        if (value[0][1] == null){
+            this.props.navigation.navigate("Login");
+        }else {
+          this.props.checkToken(value);
         }
-    }
+      },
+      err => {
+        console.log(err)
+        this.props.navigation.navigate("Login");
+      }
+    );
+  }
 
-    render(){
-        return <Loading/>
+  componentWillReceiveProps(props) {
+    if (props.valid) {
+      this.props.navigation.navigate("Views");
+    } else {
+      this.props.navigation.navigate("Login");
     }
+  }
+
+  render() {
+    return <Loading />;
+  }
 }
 
-const mapStateToProps=(store)=>{
-    return{
-        login:store.login
-    }
-}
+const mapStateToProps = store => {
+  return {
+    valid: store.login.valid
+  };
+};
 
-const mapDispatchToProsp=(dispatch)=>{
-    return bindActionCreators({exchangeToken},dispatch);
-}
+const mapDispatchToProsp = dispatch => {
+  return bindActionCreators({ checkToken }, dispatch);
+};
 
-const ConnectMainToRedux = connect(mapStateToProps,mapDispatchToProsp)(Main);
+const ConnectMainToRedux = connect(
+  mapStateToProps,
+  mapDispatchToProsp
+)(Main);
 
-const RootNavigation = createSwitchNavigator({
-    Main:{
-        screen:ConnectMainToRedux,
+const RootNavigation = createSwitchNavigator(
+  {
+    Main: {
+      screen: ConnectMainToRedux
     },
-    Views:{
-      screen:Views,
+    Views: {
+      screen: Views
     },
-    Login:{
-      screen:Login,
+    Login: {
+      screen: Login
     }
-  },{
-    initialRouteName: 'Main',
-});
+  },
+  {
+    initialRouteName: "Main"
+  }
+);
 
-export default AppContainer = createAppContainer(RootNavigation);
+export default (AppContainer = createAppContainer(RootNavigation));
