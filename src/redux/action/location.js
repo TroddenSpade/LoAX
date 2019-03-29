a8a669749c3df36b60cbd918925de99ea15e3c4c
troddenspade <parsasam1378@gmail.com>
2019-03-29 11:57:22 +0430

    location function added

--- src/redux/action/location.js
+++ src/redux/action/location.js
@@ -1,27 +1,11 @@
-// import * as firebase from 'firebase';
+import axios from "axios";
 
-// export const location =(geoHash)=>{
-//     return (dispatch,getState)=>{
-//         dispatch({type:'LOADING_START'});
-//         let posts=[];
-//         firebase.database().ref(`post`)
-//         .orderByChild(`geoHash`)
-//         .once(`value`)
-//         .then(async (snapshot) => {
-//             for(let key in snapshot.val()){
-//                 await firebase.database().ref(`user/${snapshot.val()[key].userid}`)
-//                 .once(`value`)
-//                 .then(async (snapshot2)=>{
-//                     posts.push({
-//                         ...snapshot.val()[key],
-//                         "username":snapshot2.val().username,
-//                         "avatar":snapshot2.val().pic,
-//                     })
-//                     return snapshot2
-//                 })
-//             }
-//             return posts
-//         }).then((response)=>dispatch({type:'LOCATION_POSTS_SUCCESS',payload:response}))
-//         .catch((e)=>console.log(e))
-//     }
-// }
+import { LOCATION } from '../../utils/links';
+
+export const location = (hash) => {
+    return (dispatch,getState)=>{
+        axios.get(`${LOCATION}?hash=${hash}&order=desc&limit=5&skip=0`)
+        .then(response => dispatch({type:'LOCATION_SUCCESS',payload:response.data}) )
+        .catch(err=>console.log(err))
+    }
+};
