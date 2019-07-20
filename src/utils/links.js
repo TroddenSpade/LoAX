afd769553de82f00fb7fb56d6bb582bdbdc7cbed
TroddenSpade <parsasam1378@gmail.com>
2019-07-20 13:22:24 +0430

    links added

---
+++ src/utils/links.js
@@ -0,0 +1,36 @@
+export const FireBase=`https://loaxapp.firebaseio.com/`;
+export const FireBaseUser =`https://loaxapp.firebaseio.com/user`;
+export const FireBasePost = `https://loaxapp.firebaseio.com/post`;
+
+export const GeoCodeAPI = `AIzaSyBlhgW_l2i-Q3uYFegQ7ZeHulkOkPLqP3I`;
+export const API =`AIzaSyA7apIsKFgo3d0TnCW9hpAzU3sH9kv8MJU`;
+export const OpenCageAPI = `004dae84be354272a7986ad80d80f55b`;
+export const OpenCage = `https://api.opencagedata.com/geocode/v1/json`;
+
+export const Register =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=`;
+export const Signin =`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=`;
+export const Exchange =`https://securetoken.googleapis.com/v1/token?key=`;
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA7apIsKFgo3d0TnCW9hpAzU3sH9kv8MJU",
+    authDomain: "loax-app.firebaseapp.com",
+    databaseURL: "https://loaxapp.firebaseio.com",
+    storageBucket: "loaxapp.appspot.com"
+};
+
+export const uiAvatar =`https://ui-avatars.com/api/?background=225377&name=`;
+
+const SERVER = `http://198.143.181.77:3000`;
+export const SIGN_UP = `${SERVER}/api/signup`;
+export const SIGN_IN = `${SERVER}/api/signin`;
+export const CHECK_TOKEN = `${SERVER}/api/checktoken`;
+export const GET_POSTS = `${SERVER}/api/posts`;
+export const GET_MY_POSTS = `${SERVER}/api/myposts`;
+export const GET_USER_POSTS =`${SERVER}/api/getuserposts`;
+export const GET_USER_DATA =`${SERVER}/api/getuser`;
+export const SEARCH = `${SERVER}/api/search`;
+export const ADD_POST = `${SERVER}/api/addpost`;
+export const USER_UPDATE = `${SERVER}/api/userupdate`;
+export const DELETE_POST = `${SERVER}/api/deletepost`;
+export const REPORT = `${SERVER}/api/report`;
+export const LOCATION = `${SERVER}/api/location`;
